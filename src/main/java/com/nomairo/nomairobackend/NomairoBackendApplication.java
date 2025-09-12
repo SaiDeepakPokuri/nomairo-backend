6af0b41d9295e7f7178309a05e2902f19a76c76a
bablu <saideepak.pokuri@gmail.com>
2025-09-12 18:28:07 +0530

    Initial commit

---
+++ src/main/java/com/nomairo/nomairobackend/NomairoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nomairo.nomairobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomairoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomairoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nomairo/nomairobackend/NomairoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomairo.nomairobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomairoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
