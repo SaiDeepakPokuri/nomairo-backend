ec1756b004ed571c3d7b11222c37f376750f9fc1
bablu <saideepak.pokuri@gmail.com>
2025-09-12 22:05:24 +0530

    created domain

--- src/main/java/com/nomairo/nomairobackend/Domain/MyUser.java
+++ src/main/java/com/nomairo/nomairobackend/Domain/MyUser.java
@@ -1,5 +1,24 @@
 package com.nomairo.nomairobackend.Domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MyUser {
-    
+    @Id
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String role;
 }
